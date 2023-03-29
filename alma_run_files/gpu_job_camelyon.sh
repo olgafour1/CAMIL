ef1bdf11dd32d5369e79aec45583739fbbb2dde6
admin_ofourkioti <admin_ofourkioti@30357CMBDTL.icr.ac.uk>
2023-03-29 20:07:43 +0100

    k 10

--- alma_run_files/gpu_job_camelyon.sh
+++ alma_run_files/gpu_job_camelyon.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --job-name=test_gpu
-#SBATCH --output=/home/ofourkioti/Projects/Neighbor_constrained_attention_based_MIL/camelyon_results/nystromformer_k_2.txt
+#SBATCH --output=/home/ofourkioti/Projects/Neighbor_constrained_attention_based_MIL/camelyon_results/nystromformer_k_10.txt
 #SBATCH --error=/home/ofourkioti/Projects/Neighbor_constrained_attention_based_MIL/camelyon_results/error.err
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=6
@@ -13,5 +13,5 @@ source /opt/software/applications/anaconda/3/etc/profile.d/conda.sh
 conda activate exp_env
 cd /home/ofourkioti/Projects/Neighbor_constrained_attention_based_MIL/
 
-python run.py  --k 2 --experiment_name nystromformer_k_2  --feature_path /data/scratch/DBI/DUDBI/DYNCESYS/OlgaF/multi_magnification_project/camelyon_data/tf_feats_256/resnet_feats/h5_files --label_file label_files/camelyon_data.csv
+python run.py  --k 10 --experiment_name nystromformer_k_10  --feature_path /data/scratch/DBI/DUDBI/DYNCESYS/OlgaF/multi_magnification_project/camelyon_data/tf_feats_256/resnet_feats/h5_files --label_file label_files/camelyon_data.csv
 #python run_simclr.py --simclr_path  lipo_SIMCLR_checkpoints --feature_path /data/scratch/DBI/DUDBI/DYNCESYS/OlgaF/multi_magnification_project/SAR_data/simclr_imgs/h5_files/  --csv_file lipo_csv_files/splits_0.csv --simclr_batch_size 1024
