e81cdb95cb035766a6ae9ecd47ffe70a9a2f9cb1
admin_ofourkioti <admin_ofourkioti@30357CMBDTL.icr.ac.uk>
2023-03-31 20:14:39 +0100

    k 8

--- alma_run_files/gpu_job_tcga.sh
+++ alma_run_files/gpu_job_tcga.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --job-name=test_gpu
-#SBATCH --output=/home/ofourkioti/Projects/Neighbor_constrained_attention_based_MIL/tcga_results/nystromformer_k_4.txt
+#SBATCH --output=/home/ofourkioti/Projects/Neighbor_constrained_attention_based_MIL/tcga_results/nystromformer_k_3.txt
 #SBATCH --error=/home/ofourkioti/Projects/Neighbor_constrained_attention_based_MIL/tcga_results/error.err
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=6
@@ -13,5 +13,5 @@ source /opt/software/applications/anaconda/3/etc/profile.d/conda.sh
 conda activate exp_env
 cd /home/ofourkioti/Projects/Neighbor_constrained_attention_based_MIL/
 
-python run.py --epochs 100 --experiment_name nystromformer_k_4  --k 4 --dataset tcga --feature_path /data/scratch/DBI/DUDBI/DYNCESYS/OlgaF/TCGA_data/features/h5_files  --label_file label_files/tcga_data.csv --csv_files tcga_csv_files --save_dir TCGA_Saved_model
+python run.py --epochs 100 --experiment_name nystromformer_k_3  --k 3 --dataset tcga --feature_path /data/scratch/DBI/DUDBI/DYNCESYS/OlgaF/TCGA_data/features/h5_files  --label_file label_files/tcga_data.csv --csv_files tcga_csv_files --save_dir TCGA_Saved_model
 
