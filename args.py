57c937fccd6654c4e9edf93ef591ca686787dcdf
admin_ofourkioti <admin_ofourkioti@30357CMBDTL.icr.ac.uk>
2023-04-14 15:16:21 +0100

    for loop bash

--- args.py
+++ args.py
@@ -5,6 +5,8 @@ import numpy as np
 import tensorflow as tf
 
 
+
+
 def parse_args():
     """Parse input arguments.
     Parameters
@@ -17,74 +19,30 @@ def parse_args():
     """
     parser = argparse.ArgumentParser(description='Train Graph Att net')
 
-    parser.add_argument('--dataset', dest='dataset',
-                        help='select dataset',
-                        choices=["camelyon","tcga", 'sarcoma'],
-                        default="camelyon", type=str)
-    parser.add_argument('--save_dir', dest='save_dir',
-                        help='directory where the weights of the model are stored',
-                        default="camelyon_Saved_model", type=str)
-    parser.add_argument('--simclr_path', dest='simclr_path',
-                        help='directory where the images are stored',
-                        default="camelyon_SIMCLR_checkpoints", type=str)
-    parser.add_argument('--lr', dest='init_lr',
-                        help='initial learning rate',
-                        default=0.0002, type=float)
-    parser.add_argument('--decay', dest='weight_decay',
-                        help='weight decay',
-                        default=1e-5, type=float)
-    parser.add_argument('--momentum', dest='momentum',
-                        help='momentum',
-                        default=0.9, type=float)
-    parser.add_argument('--epochs', dest='epochs',
-                        help='number of epochs to train GRAPH MIL',
-                        default=50, type=int)
-    parser.add_argument('--seed_value', dest='seed_value',
-                        help='use same seed value for reproducability',
-                        default=12321, type=int)
-    parser.add_argument('--run', dest='run',
-                        help='number of experiments to be run',
-                        default=1, type=int)
-    parser.add_argument('--k', dest='k',
-                        help='number of neighbors taken into account',
-                        default=3, type=int,
-                        choices=range(1,12), metavar="[1-12]")
-    parser.add_argument('--feature_path', dest='feature_path',
-                        help='directory where the images are stored',
-                        default='/home/admin_ofourkioti/Documents/camelyon/resnet_feats/h5_files/', type=str)
-    parser.add_argument('--dataset_path', dest='data_path',
-                        help='directory where the images are stored',
-                        default="",
-                        type=str)
-    parser.add_argument('--experiment_name', dest='experiment_name',
-                        help='the name of the experiment needed for the logs',
-                        default="test", type=str)
-    parser.add_argument('--simclr_batch_size', dest='simclr_batch_size',
-                        help='batch size used bu the siamese network',
-                        default=512, type=int)
-    parser.add_argument('--simclr_epochs', dest='simclr_epochs',
-                        help='epochs for siamese network',
-                        default=50, type=int)
-    parser.add_argument('--input_shape', dest="input_shape",
-                        help='shape of the image',
-                        default=(1024,), type=int, nargs=3)
-    parser.add_argument('--label_file', dest="label_file",
-                        help='csv file with information about the labels',
-                       default="label_files/camelyon_data.csv",type=str)
-    parser.add_argument('--csv_files', dest="csv_files",
-                        help='csv file with information about the labels',
-                       default="camelyon_csv_files",type=str)
-    parser.add_argument('--raw_save_dir', dest="raw_save_dir",
-                        help='directory where the attention weights are saved',
-                        default="heatmaps", type=str)
-    parser.add_argument('--retrain', dest="retrain",
-                        action='store_true', default=False)
-    parser.add_argument('--save_exp_code', type=str, default=None,
-                        help='experiment code')
+    parser.add_argument('--dataset', dest='dataset',help='select dataset',choices=["camelyon","tcga", 'sarcoma'],default="camelyon", type=str)
+    parser.add_argument('--save_dir', dest='save_dir',help='directory where the weights of the model are stored',default="camelyon_Saved_model", type=str)
+    parser.add_argument('--simclr_path', dest='simclr_path',help='directory where the images are stored',default="camelyon_SIMCLR_checkpoints", type=str)
+    parser.add_argument('--lr', dest='init_lr',help='initial learning rate',default=0.0002, type=float)
+    parser.add_argument('--decay', dest='weight_decay',help='weight decay',default=1e-5, type=float)
+    parser.add_argument('--momentum', dest='momentum',help='momentum',default=0.9, type=float)
+    parser.add_argument('--epochs', dest='epochs',help='number of epochs to train GRAPH MIL',default=50, type=int)
+    parser.add_argument('--seed_value', dest='seed_value',help='use same seed value for reproducability',default=12321, type=int)
+    parser.add_argument('--run', dest='run',help='number of experiments to be run',default=1, type=int)
+    parser.add_argument('--k', dest='k',help='number of neighbors taken into account',default=3, type=int,choices=range(1,12), metavar="[1-12]")
+    parser.add_argument('--feature_path', dest='feature_path',help='directory where the images are stored',default='/home/admin_ofourkioti/Documents/camelyon/resnet_feats/h5_files/', type=str)
+    parser.add_argument('--dataset_path', dest='data_path',help='directory where the images are stored',default="",type=str)
+    parser.add_argument('--experiment_name', dest='experiment_name',help='the name of the experiment needed for the logs',default="test", type=str)
+    parser.add_argument('--simclr_batch_size', dest='simclr_batch_size',help='batch size used bu the siamese network',default=512, type=int)
+    parser.add_argument('--simclr_epochs', dest='simclr_epochs',help='epochs for siamese network',default=50, type=int)
+    parser.add_argument('--input_shape', dest="input_shape",help='shape of the image',default=(1024,), type=int, nargs=3)
+    parser.add_argument('--label_file', dest="label_file",help='csv file with information about the labels',default="label_files/camelyon_data.csv",type=str)
+    parser.add_argument('--csv_file', dest="csv_file",help='csv file with information about the labels',default="camelyon_csv_files/splits_0.csv",type=str)
+    parser.add_argument('--raw_save_dir', dest="raw_save_dir",help='directory where the attention weights are saved',default="heatmaps", type=str)
+    parser.add_argument('--retrain', dest="retrain",action='store_true', default=False)
+    parser.add_argument('--save_exp_code', type=str, default=None,help='experiment code')
     parser.add_argument('--overlap', type=float, default=None)
     parser.add_argument('--config_file', type=str, default="heatmap_config_template.yaml")
-    parser.add_argument('--subtyping', dest="subtyping",
-                        action='store_true', default=False)
+    parser.add_argument('--subtyping', dest="subtyping",action='store_true', default=False)
 
 
     args = parser.parse_args()
--- run.py
+++ run.py
@@ -29,61 +29,43 @@ if __name__ == "__main__":
     adj_dim = None
     set_seed(12321)
 
-    csv_files=args.csv_files
+    csv_file=args.csv_file
     acc=[]
     recall=[]
     f_score=[]
     auc=[]
     precision=[]
 
+    fold_id = os.path.splitext(csv_file)[0].split("_")[1]
 
-    for csv_file in sorted(os.listdir(csv_files)):
+    os.makedirs(os.path.join(args.simclr_path, "fold_{}".format(fold_id)), exist_ok=True)
 
+    references = pd.read_csv(csv_file)
 
-            fold_id = os.path.splitext(csv_file)[0].split("_")[1]
-
-            os.makedirs(os.path.join(args.simclr_path, "fold_{}".format(fold_id)), exist_ok=True)
-
-            references=pd.read_csv(os.path.join(csv_files,csv_file))
-
-            train_bags= references["train"].apply(lambda x:os.path.join(args.feature_path,x+".h5")).values.tolist()
-
-            def func_val(x):
-                value = None
-                if isinstance(x, str):
-                        value = os.path.join(args.feature_path, x + ".h5")
-                return value
-
-            val_bags =  references.apply(lambda row: func_val(row.val), axis=1).dropna().values.tolist()
-
-            test_bags = references.apply(lambda row: func_val(row.test), axis=1).dropna().values.tolist()
-
-            train_net = CHARM(args)
-            train_net.train(train_bags, fold_id, val_bags, args)
-
-            test_net = CHARM(args)
-
-            test_acc, test_auc, test_precision, test_recall, test_f_score = test_net.predict(test_bags,
-                                                                                                 fold_id,
-                                                                                                 args,
-                                                                                               test_model=test_net.model)
-            acc.append(test_acc)
-            recall.append(test_recall)
-            f_score.append(test_f_score)
-            auc.append(test_auc)
-            precision.append(test_precision)
+    train_bags = references["train"].apply(lambda x: os.path.join(args.feature_path, x + ".h5")).values.tolist()
 
 
-    print('mean accuracy = ', np.mean(acc))
-    print('std = ', np.std(acc))
-    print(' mean precision = ', np.mean(precision))
-    print('std = ', np.std(precision))
-    print('mean recall = ', np.mean(recall))
-    print('std = ', np.std(recall))
-    print(' mean fscore = ', np.mean(f_score))
-    print('std = ', np.std(f_score))
-    print(' mean auc = ', np.mean(auc))
-    print('std = ', np.std(auc))
+    def func_val(x):
+        value = None
+        if isinstance(x, str):
+            value = os.path.join(args.feature_path, x + ".h5")
+        return value
+
+
+    val_bags = references.apply(lambda row: func_val(row.val), axis=1).dropna().values.tolist()
+
+    test_bags = references.apply(lambda row: func_val(row.test), axis=1).dropna().values.tolist()
+
+    train_net = CHARM(args)
+
+    train_net.train(train_bags, fold_id, val_bags, args)
+
+    test_net = CHARM(args)
+
+    test_acc, test_auc, test_f_score = test_net.predict(test_bags,
+                                                        fold_id,
+                                                        args,
+                                                        test_model=test_net.model)
 
 
 
