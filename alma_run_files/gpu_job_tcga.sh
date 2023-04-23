c09521cf965f13aab0c0b2ff249b4d67dc0832e2
admin_ofourkioti <admin_ofourkioti@30357CMBDTL.icr.ac.uk>
2023-04-23 22:06:39 +0100

    alma

--- alma_run_files/gpu_job_tcga.sh
+++ alma_run_files/gpu_job_tcga.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --job-name=test_gpu
-#SBATCH --output=/home/ofourkioti/Projects/Neighbor_constrained_attention_based_MIL/tcga_results/nystromformer_k_4.txt
+#SBATCH --output=/home/ofourkioti/Projects/Neighbor_constrained_attention_based_MIL/tcga_results/nystromformer_k_8.txt
 #SBATCH --error=/home/ofourkioti/Projects/Neighbor_constrained_attention_based_MIL/tcga_results/error.err
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=6
@@ -18,6 +18,6 @@ cd /home/ofourkioti/Projects/Neighbor_constrained_attention_based_MIL/
 #python run.py --epochs 100 --experiment_name nystromformer_k_2  --k 2 --dataset tcga --feature_path /data/scratch/DBI/DUDBI/DYNCESYS/OlgaF/TCGA_data/features/h5_files  --label_file label_files/tcga_data.csv --csv_files tcga_csv_files --save_dir TCGA_Saved_model
 
 for i in {0..4};
-do python run.py --epochs 100 --experiment_name nystromformer_k_4  --k 4 --dataset tcga --feature_path /data/scratch/DBI/DUDBI/DYNCESYS/OlgaF/TCGA_data/features/h5_files  --label_file label_files/tcga_data.csv --csv_file tcga_csv_files/splits_${i}.csv --save_dir TCGA_Saved_model;
+do python run.py --epochs 100 --experiment_name nystromformer_k_8  --k 8 --dataset tcga --feature_path /data/scratch/DBI/DUDBI/DYNCESYS/OlgaF/TCGA_data/features/h5_files  --label_file label_files/tcga_data.csv --csv_file tcga_csv_files/splits_${i}.csv --save_dir TCGA_Saved_model;
 done
 #python run_simclr.py --simclr_path  lipo_SIMCLR_checkpoints --feature_path /data/scratch/DBI/DUDBI/DYNCESYS/OlgaF/multi_magnification_project/SAR_data/simclr_imgs/h5_files/  --csv_file lipo_csv_files/splits_0.csv --simclr_batch_size 1024
