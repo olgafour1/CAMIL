f609772ede2cd4bdb3732838b48ac4ccb09ed6b3
admin_ofourkioti <admin_ofourkioti@30357CMBDTL.icr.ac.uk>
2023-06-26 12:24:14 +0100

    ablation studies

--- training/charm.py
+++ training/charm.py
@@ -46,22 +46,24 @@ class CHARM:
 
         dense = self.inputs['bag']
 
-        encoder_output = tf.squeeze(self.nyst_att(tf.expand_dims(dense, axis=0)))
-        encoder_output = tf.ensure_shape(encoder_output, [None, 512])
-
-        encoder_output =   encoder_output + dense
-
-        # attention_matrix = CustomAttention(weight_params_dim=256)(encoder_output)
-        # norm_alpha, alpha = NeighborAggregator(output_dim=1, name="alpha")(
-        #     [attention_matrix, self.inputs["adjacency_matrix"]])
-        # value = self.wv(dense)
-        # xl = multiply([norm_alpha, value], name="mul_1")
+        # encoder_output = tf.squeeze(self.nyst_att(tf.expand_dims(dense, axis=0)))
+        # encoder_output = tf.ensure_shape(encoder_output, [None, 512])
         #
-        # xo = xl + encoder_output
+        # encoder_output =   encoder_output + dense
 
-        k_alpha = self.attcls(encoder_output)
+        encoder_output = dense
 
-        attn_output = tf.keras.layers.multiply([k_alpha, encoder_output])
+        attention_matrix = CustomAttention(weight_params_dim=256)(encoder_output)
+        norm_alpha, alpha = NeighborAggregator(output_dim=1, name="alpha")(
+            [attention_matrix, self.inputs["adjacency_matrix"]])
+        value = self.wv(dense)
+        xl = multiply([norm_alpha, value], name="mul_1")
+
+        xo = xl + encoder_output
+
+        k_alpha = self.attcls(xo)
+
+        attn_output = tf.keras.layers.multiply([k_alpha, xo])
 
         out = Last_Sigmoid(output_dim=1,
                            name='FC1_sigmoid_1',
