c0a0d8262f432d4709922b57e8cba0eec6cbacb2
admin_ofourkioti <admin_ofourkioti@30357CMBDTL.icr.ac.uk>
2023-06-27 19:57:27 +0100

    run cam_17 experiment

--- alma_run_files/gpu_cam_17.sh
+++ alma_run_files/gpu_cam_17.sh
@@ -11,7 +11,7 @@
 module load anaconda/3
 source /opt/software/applications/anaconda/3/etc/profile.d/conda.sh
 conda activate openslide
-cd /home/mvries/SAD_MIL/
+cd /home/ofourkioti/Projects/Neighbor_constrained_attention_based_MIL/
 
 #python run.py --dataset camelyon --experiment_name phi --feature_path /data/scratch/DBI/DUDBI/DYNCESYS/OlgaF/multi_magnification_project/camelyon_data/tf_feats_256/resnet_feats/h5_files --label_file label_files/camelyon_data.csv --csv_files camelyon_csv_files/splits_0.csv  --epoch 100 --save_dir cam_Saved_model --lambda1 0.00001
 #python run_simclr.py --simclr_path  lipo_SIMCLR_checkpoints --feature_path /data/scratch/DBI/DUDBI/DYNCESYS/OlgaF/multi_magnification_project/SAR_data/simclr_imgs/h5_files/  --csv_file lipo_csv_files/splits_0.csv --simclr_batch_size 1024
