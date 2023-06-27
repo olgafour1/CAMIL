c5e7c0abcf68fa2c7d1e815942a8a6f43f1d4d3d
admin_ofourkioti <admin_ofourkioti@30357CMBDTL.icr.ac.uk>
2023-06-27 20:00:23 +0100

    run cam_17 experiment

--- alma_run_files/gpu_cam_17.sh
+++ alma_run_files/gpu_cam_17.sh
@@ -16,6 +16,6 @@ cd /home/ofourkioti/Projects/Neighbor_constrained_attention_based_MIL/
 #python run.py --dataset camelyon --experiment_name phi --feature_path /data/scratch/DBI/DUDBI/DYNCESYS/OlgaF/multi_magnification_project/camelyon_data/tf_feats_256/resnet_feats/h5_files --label_file label_files/camelyon_data.csv --csv_files camelyon_csv_files/splits_0.csv  --epoch 100 --save_dir cam_Saved_model --lambda1 0.00001
 #python run_simclr.py --simclr_path  lipo_SIMCLR_checkpoints --feature_path /data/scratch/DBI/DUDBI/DYNCESYS/OlgaF/multi_magnification_project/SAR_data/simclr_imgs/h5_files/  --csv_file lipo_csv_files/splits_0.csv --simclr_batch_size 1024
 for i in {0..4};
-do python run.py --experiment_name cam_17  --feature_path /data/scratch/DBI/DUDBI/DYNCESYS/OlgaF/tmi/cam-17/feats/h5_files/ --label_file label_files/camelyon_17.csv --csv_file camelyon_17_splits/splits_${i}.csv --lambda1 1 --epoch 200 --save_dir cam17_Saved_model
+do python run.py --experiment_name cam_17  --feature_path /data/scratch/DBI/DUDBI/DYNCESYS/OlgaF/tmi/cam-17/feats/h5_files/ --label_file label_files/camelyon_17.csv --csv_file camelyon_17_splits/splits_${i}.csv  --epoch 200 --save_dir cam17_Saved_model
 done
 
