467363ba446cff51db3c4ecaa88b6c4edb46e60c
admin_ofourkioti <admin_ofourkioti@30357CMBDTL.icr.ac.uk>
2023-07-04 16:16:27 +0100

    run cam_17 experiment

--- training/charm.py
+++ training/charm.py
@@ -47,22 +47,22 @@ class CHARM:
 
         dense = Dense(512, activation='relu')(self.inputs['bag'])
 
-        # encoder_output = tf.squeeze(self.nyst_att(tf.expand_dims(dense, axis=0)))
-        # encoder_output = tf.ensure_shape(encoder_output, [None, 512])
-        #
-        # encoder_output =  encoder_output + dense
-        #
-        # attention_matrix = CustomAttention(weight_params_dim=256)(encoder_output)
-        # norm_alpha, alpha = NeighborAggregator(output_dim=1, name="alpha")(
-        #     [attention_matrix, self.inputs["adjacency_matrix"]])
-        # value = self.wv(dense)
-        # xl = multiply([norm_alpha, value], name="mul_1")
-        #
-        # xo = xl + encoder_output
+        encoder_output = tf.squeeze(self.nyst_att(tf.expand_dims(dense, axis=0)))
+        encoder_output = tf.ensure_shape(encoder_output, [None, 512])
 
-        k_alpha = self.attcls(dense)
+        encoder_output =  encoder_output + dense
 
-        attn_output = tf.keras.layers.multiply([k_alpha, dense])
+        attention_matrix = CustomAttention(weight_params_dim=256)(encoder_output)
+        norm_alpha, alpha = NeighborAggregator(output_dim=1, name="alpha")(
+            [attention_matrix, self.inputs["adjacency_matrix"]])
+        value = self.wv(dense)
+        xl = multiply([norm_alpha, value], name="mul_1")
+
+        xo = xl + encoder_output
+
+        k_alpha = self.attcls(xo)
+
+        attn_output = tf.keras.layers.multiply([k_alpha, xo])
 
         out = Last_Sigmoid(output_dim=1,
                            name='FC1_sigmoid_1',
@@ -70,7 +70,7 @@ class CHARM:
                            pooling_mode='sum',
                            subtyping=False)(attn_output)
 
-        self.net = Model(inputs=[self.inputs['bag'], self.inputs["adjacency_matrix"]], outputs=[out, dense, k_alpha])
+        self.net = Model(inputs=[self.inputs['bag'], self.inputs["adjacency_matrix"]], outputs=[out, k_alpha, k_alpha])
 
     @property
     def model(self):
